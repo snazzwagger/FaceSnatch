a70881960c9c66dac064f7bb354283b4ea1564a4
John Stanford <johnstanford@Johns-MacBook-Pro.local>
2016-10-12 16:37:44 -0400

    initial commit

---
+++ ProjectFaceSnatch/ProjectFaceSnatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectFaceSnatch
+//
+//  Created by John Stanford on 10/12/16.
+//  Copyright © 2016 Khelion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
